6b22d15156689e293824e22ab052e21869d922fc
georgecode <georgeczurlanis@gmail.com>
2017-01-15 22:54:14 -0800

    Set up envirnment bcrypt set up in app/models/user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,20 @@
 class User < ActiveRecord::Base
   # Remember to create a migration!
-end
+  # has_many :Subscriptions
+  # has_many :channels
+
+  def password
+    @password ||= BCrypt::Password.new(hashed_password)
+  end
+
+  def password=(new_password)
+    @password = BCrypt::Password.create(new_password)
+    self.hashed_password = @password
+  end
+end#End User
+
+#$cd Desktop/devbootcamp/onSite/week4/assesmentTry2/crud-and-rest-checkpoint-challenge/
+
+# user = User.new(:first_name=>'George',:last_name=>'Czu',:email=>'george@email.com', :password => 'password')
+
+# user = User.new(:first_name=>'Dana',:last_name=>'Donel',:email=>'dana@email.com', :password => 'password')
